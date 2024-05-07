087a83a3a09ea242d7137e772a0ddcea156404d4
Takuya <c0b23056c0@edu.teu.ac.jp>
2024-05-07 14:52:23 +0900

    練習３：喜ぶこうかとん

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -160,11 +160,12 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
-        
         if not (beam is None or bomb is None):
             if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
                 beam = None
                 bomb = None
+                bird.change_img(6, screen)
+                pg.display.update()
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
